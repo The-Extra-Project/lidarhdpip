c70ee793b47f1f07e31e9dd9a97db51d7c45f83b
jpirsch <jeanpirsch@gmail.com>
2023-06-21 18:43:44 +0200

    Creation of dev branch, rewrote run.sh in main.py, pdal json pipeline generation ongoing, checklist in todo.md

--- src/main.py
+++ src/main.py
@@ -4,44 +4,64 @@ import geopandas as gpd
 import json
 from shapely.geometry import Polygon, LineString, Point
 import os
+from subprocess import Popen
 
-## Pipeline creation
-def main():
-
+def get_tile_url_and_fname():
     parser=argparse.ArgumentParser(description="take a gps coordinate and produce a 3Dtile")
     parser.add_argument("coordX")
     parser.add_argument("coordY")
     args=parser.parse_args()
+    print( "Running with X={}, Y={}".format( args.coordX, args.coordY ) )
 
     fp = "/usr/src/app/georender/datas/TA_diff_pkk_lidarhd.shp"
     data = gpd.read_file(fp)
-    center = Point(806527.23,6301959.36)
+    #center = Point(806527.23,6301959.36) # for demo
     center = Point(float(args.coordX),float(args.coordY))
 
     out = data.intersects(center)
     res = data.loc[out]
     laz_path = res["url_telech"].to_numpy()[0]#.replace("$","\$")#.replace("\\\\","\\")
-    fname = res["nom_pkk"].to_numpy()[0] + ".7z"
+    dirname = res["nom_pkk"].to_numpy()[0]
+    fname = dirname + ".7z"
 
-    # # Actually the pipeline is useless beacause it 
-    # pipeline = [
-    #     {
-    #         "type": "readers.las",
-    #         "filename": fname ,
-    #             "tag": "laz",
-    #          "default_srs":"EPSG:2154"
-    #     },
-    #     {
-    #       "type": "writers.las",
-    #       "filename": "result.las"
-    #     }
-    # ]
-    # with open("full_pipeline.json", "w") as outfile:
-    #     json.dump(pipeline,outfile)
-    with open("filepath.txt", "w") as outfile:
-        outfile.write(laz_path)
-    with open("filename.txt", "w") as outfile:
-        outfile.write(fname)
+    #with open("filepath.txt", "w") as outfile:
+    #    outfile.write(laz_path)
+    #with open("filename.txt", "w") as outfile:
+    #    outfile.write(fname)
+    return laz_path, fname, dirname
+
+def generate_pdal_pipeline( dirname ):
+    # List files extracted
+    file_list = os.listdir( dirname )
+
+    def_srs = "EPSG:2154"
+    
+    # Open template file to get the pipeline struct 
+    with open( "pipeline_template.json", 'r' ) as file_pipe_in:
+        file_str = file_pipe_in.read()
+    
+    pdal_pipeline = json.loads( file_str )
+    
+    las_reader = { "type": "reader.las", "filename": "filename.laz", "tag": "filename", "default_srs":def_srs }
+    
+    pdal_pipeline['pipeline'].insert( 0, las_reader )
+    
+    with open( "pipeline_gen.json", "w" ) as file_pipe_out:
+        json.dump( pdal_pipeline, file_pipe_out )
+
+
+## Pipeline creation
+def main():
+    # Uses geopanda and shapely to intersect gps coord with available laz tiles files
+    # Returns corresponding download url and filename
+    laz_path, fname, dirname = get_tile_url_and_fname()
+
+    # Download laz tile file from ign
+    #Popen( ["wget", "--user-agent=Mozilla/5.0", laz_path] )
+    # Extract it
+    Popen( ["7z", "x", fname] ) 
+
+    generate_pdal_pipeline( dirname )
 
 if __name__ == '__main__':
     main()

edd5afe52d3ac966660db982cd4072dc56833da1
jean <jeanpirsch@gmail.com>
2023-07-04 18:18:06 +0200

    Automating pipeline from gps coordinate conversion to pdal laz file merge.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,42 @@ import geopandas as gpd
 import json
 from shapely.geometry import Polygon, LineString, Point
 import os
-from subprocess import Popen
+import requests
+
+def create_bounding_box(latitude_max: int, lattitude_min: int, longitude_max: int, longitude_min: int):
+    """
+    Create a bounding box from 4 coordinates
+    """
+    return Polygon([(longitude_min, lattitude_min), (longitude_max, lattitude_min), (longitude_max, latitude_max), (longitude_min, latitude_max), (longitude_min, lattitude_min)])
+
+
+
+def get_tile_url_and_fname_from_polygon():
+    parser=argparse.ArgumentParser(description="take a bounded box details and produce a 3D tile")
+    parser.add_argument("lattitude max")
+    parser.add_argument("lattitude min")
+    parser.add_argument("longotude max")
+    parser.add_argument("longotude min")
+    args = parser.parse_args()
+    print( "Running with lat_max={}, lat_min={}, long_max={}, long_min={}".format( args.lattitude_max, args.lattitude_min, args.longotude_max, args.longotude_min ) )
+
+
+    fp = "/usr/src/app/georender/datas/TA_diff_pkk_lidarhd.shp"
+    data = gpd.read_file(fp)
+
+    polygonRegion = create_bounding_box(args.lattitude_max, args.lattitude_min, args.longotude_max, args.longotude_min)
+    out = data.intersects(polygonRegion)
+    res = data.loc[out]
+    laz_path = res["url_telech"].to_numpy()[0]#.replace("$","\$")#.replace("\\\\","\\")
+    dirname = res["nom_pkk"].to_numpy()[0]
+    fname = dirname + ".7z"
+
+    return laz_path, fname, dirname
+
+
+
+
+
 
 def get_tile_url_and_fname():
     parser=argparse.ArgumentParser(description="take a gps coordinate and produce a 3Dtile")
@@ -15,8 +50,16 @@ def get_tile_url_and_fname():
 
     fp = "/usr/src/app/georender/datas/TA_diff_pkk_lidarhd.shp"
     data = gpd.read_file(fp)
+
+    # todo : how to avoid rewriting too much code between bounding box mode and center mode ?
+    # todo : Document input format and pyproj conversion
+    # see link 1 ( to epsg codes ), link 2 ( to pyproj doc )
+    from pyproj import Transformer
+    transformer = Transformer.from_crs( 'EPSG:4326', 'EPSG:2154' )
+    coordX, coordY = transformer.transform( args.coordX, args.coordY )
+
     #center = Point(806527.23,6301959.36) # for demo
-    center = Point(float(args.coordX),float(args.coordY))
+    center = Point( float(coordX), float(coordY) )
 
     out = data.intersects(center)
     res = data.loc[out]
@@ -31,20 +74,38 @@ def get_tile_url_and_fname():
     return laz_path, fname, dirname
 
 def generate_pdal_pipeline( dirname ):
-    # List files extracted
-    file_list = os.listdir( dirname )
-
-    def_srs = "EPSG:2154"
+    # Pdal pipeline is specified by a json
+    # basically it's a list of filters which can specify actions to perform
+    # each filter is a dict
     
     # Open template file to get the pipeline struct 
     with open( "pipeline_template.json", 'r' ) as file_pipe_in:
         file_str = file_pipe_in.read()
-    
+   
     pdal_pipeline = json.loads( file_str )
+
+    # List files extracted
+    # Now we don't check extension and validity
+    file_list = os.listdir( dirname )
+
+    def_srs = "EPSG:2154"
+
+    # Builds the list of dicts and filetags ( fname without ext )
+    ftags = []
+    las_readers = []
+    for fname in file_list:
+        tag = fname[:-4]
+        ftags.append( tag )
+        las_readers.append( { "type": "readers.las", "filename": fname, "tag": tag, "default_srs":def_srs } )
     
-    las_reader = { "type": "reader.las", "filename": "filename.laz", "tag": "filename", "default_srs":def_srs }
-    
-    pdal_pipeline['pipeline'].insert( 0, las_reader )
+    # Insert file tags in the list of inputs to merge
+    # Must be done before next insertion because we know the place of merge filter in the list at this moment
+    for ftag in ftags:
+        pdal_pipeline['pipeline'][0]['inputs'].insert( 0, ftag )
+
+    # Insert the list of las file readers dicts
+    for las_reader in las_readers:
+        pdal_pipeline['pipeline'].insert( 0, las_reader )
     
     with open( "pipeline_gen.json", "w" ) as file_pipe_out:
         json.dump( pdal_pipeline, file_pipe_out )
@@ -56,12 +117,34 @@ def main():
     # Returns corresponding download url and filename
     laz_path, fname, dirname = get_tile_url_and_fname()
 
+    from subprocess import check_call
+    #from subprocess import Popen
     # Download laz tile file from ign
-    #Popen( ["wget", "--user-agent=Mozilla/5.0", laz_path] )
+    check_call( ["wget", "--user-agent=Mozilla/5.0", laz_path] )
     # Extract it
-    Popen( ["7z", "x", fname] ) 
+    check_call( ["7z", "x", fname] ) 
 
     generate_pdal_pipeline( dirname )
+
+    # run pdal pipeline with the generated json :
+    os.chdir( dirname )
+    # todo : There should be further doc and conditions on this part
+    #        Like understanding why some ign files have it and some don't
+    # In case the WKT flag is not set :
+    for laz_fname in os.listdir( '.' ):
+        f = open( laz_fname, 'rb+' )
+        f.seek( 6 )
+        f.write( bytes( [17, 0, 0, 0] ) )
+        f.close()
+    check_call( ["pdal", "pipeline", "../pipeline_gen.json"] )
+    os.chdir( '..' )
+
+    #run_py3dtiles_on_result()
+
+    #upload_output_to_3dtilesrenderer()
+
+    #launch_visualisation()
+
 
 if __name__ == '__main__':
     main()

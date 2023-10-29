a8f34abc149d6abe56c048c0801cfbaaed0f5eb9
= <malikdhruv1994@gmail.com>
2023-10-29 21:18:18 +0000

    adding cron job

--- job_scheduler/route/bacalau_script.py
+++ job_scheduler/route/bacalau_script.py
@@ -5,18 +5,21 @@ This script interacts with the API gateway to
 """ 
 from fastapi import  APIRouter, Request, Response, status
 from fastapi.encoders import jsonable_encoder
+from dotenv import load_dotenv, dotenv_values
+
 import boto3
 import json
 import os
 import logging
 import requests
+
 from bacalhau_apiclient.models.storage_spec import StorageSpec
 from bacalhau_sdk.api import submit
 from bacalhau_sdk.config import get_client_id
 from bacalhau_sdk.api import results, states
 from dotenv import dotenv_values
-
-
+from fastapi_scheduler import SchedulerAdmin
+from kafka import KafkaConsumer
 logging.getLogger('boto3').setLevel(logging.CRITICAL)
 config = dotenv_values(dotenv_path='.env')
 
@@ -28,14 +31,51 @@ router = APIRouter(
     tags=["bot-service"],
     responses={404: {"description": "Not found"}},
 )
+## runs the cron job on the kafka routing for fetching the result 
+scheduler = SchedulerAdmin.bind(router)
+
+load_dotenv(dotenv_path='./../.env')
 
 
-#fargate_ecr_endpoint = ""
+
+
+
+
+def cron_job(topic_name: str):
+    """
+    this function runs the loop job in fetching the kafka topic messages in order to get the parameters and execute the pipeline
+    
+    """
+    consumer: KafkaConsumer = KafkaConsumer(
+          bootstrap_servers=['growing-krill-8066-eu1-kafka.upstash.io:9092' ],
+  sasl_mechanism='SCRAM-SHA-256',
+  security_protocol='SASL_SSL',
+  sasl_plain_username= os.getenv("SASL_PLAIN_USERNAME"),
+  sasl_plain_password=os.getenv("SASL_PLAIN_PASSWORD"),
+        auto_offset_reset='earliest',
+        consumer_timeout_ms=1000,
+        topics=['']
+    )
+    message = consumer.poll(timeout_ms=60000)
+    if message is not None:
+        
+
+
+@router.get("/requestJob/surface_reconstruction/bacalhau")
+async def runpipeline_Bacalhau(request: Request, response: Response):
+    """
+    gets the input parameters from the cronjob()
+    
+    
+    """
+    
+    while 
+
 
 @router.get("/requestJob/surface_reconstruction/ECS")
 async def runpipeline_ECS(request: Request, response: Response):
     '''
-    It parses the input parameters from requests(consisting of the parameters of the compute job). 
+    It parses the input parameters from requests(consisting of the parameters of the compute job) from the kafka queue. 
     then pulls the docker image of reconstruction-pipline and executes it on the ECS which schedules it to the bacalhau node.
     )
     
@@ -100,6 +140,8 @@ def get_state(clientID: str, response: Response):
     except SystemError as s:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {"message": str(e)}
+
+
 @router.get("/job/{job_id}")
 def getJobResults(jobId: str, response: Response):
     """
@@ -115,3 +157,6 @@ def getJobResults(jobId: str, response: Response):
     except Exception as h:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {"message": str(h)}
+
+
+scheduler.scheduled_job('cron', interval=60, func=cron_job)
